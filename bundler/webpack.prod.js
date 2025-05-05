ad85d96142113975a90d829e030041bbd789065c
hasnainbabarkhan <hasnainbabar746@gmail.com>
2025-05-06 02:24:04 +0500

    gasgasd

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,15 +1,18 @@
-const { merge } = require('webpack-merge')
-const commonConfiguration = require('./webpack.common.js')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { merge } = require('webpack-merge');
+const commonConfiguration = require('./webpack.common.js');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer'); // ✅ Add this line
 
-module.exports = merge(
-    commonConfiguration,
-    {
-        mode: 'production',
-        devtool: false,
-        plugins:
-        [
-            new CleanWebpackPlugin()
-        ]
-    }
-)
+module.exports = merge(commonConfiguration, {
+    mode: 'production',
+    devtool: false,
+    optimization: {
+        splitChunks: {
+            chunks: 'all', // ✅ Enables code splitting
+        },
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new BundleAnalyzerPlugin(), // ✅ Add this plugin
+    ],
+});
